23a917c04c75c1aebf4fa47b833fdd693ce7cb36
zhouxuan553 <zhouxuan553@pingan.com.cn>
2023-09-17 09:47:37 +0800

    modify comparison_annotation.js

--- rlhf_annotation/views/account.py
+++ rlhf_annotation/views/account.py
@@ -6,7 +6,7 @@ from flask import request, jsonify
 import datetime
 from datetime import timezone
 from ..models import User, TokenBlocklist
-from flask_jwt_extended import create_access_token,jwt_required,get_jwt
+from flask_jwt_extended import create_access_token,jwt_required,get_jwt,get_jwt_identity
 from rlhf_annotation import app,db
 from rlhf_annotation.exceptions import APIException
 
@@ -49,6 +49,14 @@ def get_userinfo():
     ret =  {'username':jwt_data['username'],'code':0}
     ret.update(**jwt_data)
     return jsonify(ret)
+
+
+@app.route('/refresh', methods=['POST'])
+@jwt_required(refresh=True)
+def refresh():
+    current_user = get_jwt_identity()
+    new_token = create_access_token(identity=current_user)
+    return jsonify({'access_token': new_token}), 200
 
 
 
